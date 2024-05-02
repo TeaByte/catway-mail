faf3022610fd5363ad70fd9d654cb29483456bc9
teabyte <yazan4code@gmail.com>
2024-05-02 12:53:36 +0300

    Fixing domain

--- src/app/mail/[mail]/page.tsx
+++ src/app/mail/[mail]/page.tsx
@@ -12,7 +12,7 @@ interface MailPageProps {
 }
 
 export default async function MailPage({ params }: MailPageProps) {
-  const mail = params.mail + "@catdns.in";
+  const mail = params.mail + "@catway.org";
   const mailData = await getMailData(mail);
   return (
     <main className="mx-4 mb-14 mt-6 flex flex-col items-center justify-center gap-6 md:mx-[200px] md:mt-10 lg:mx-[300px] xl:mx-[400px] 2xl:mx-[700px]">
