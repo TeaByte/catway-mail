631c14dbd996665520f71b3f3f7f15bcdad7e61a
teabyte <yazan4code@gmail.com>
2024-05-02 12:34:22 +0300

    Mail card added

---
+++ src/app/mail/[mail]/mail-card.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+
+interface MailCardProps {
+  id: string;
+  senderEmail: string;
+  senderName: string;
+  subject: string;
+  createdAt: Date;
+  updatedAt: Date;
+  expireAt: Date;
+}
+
+export default function MailCard({ mailData }: { mailData: MailCardProps }) {
+  return (
+    <Link
+      href={`/inbox/${mailData.id}`}
+      className="rounded-md border p-4 hover:bg-primary/20"
+    >
+      <div className="flex items-start justify-between">
+        <div className="flex flex-col">
+          <span className="truncate font-bold">{mailData.senderEmail}</span>
+          <span className="truncate text-sm">{mailData.senderName}</span>
+        </div>
+        <span className="text-sm font-semibold">
+          {mailData.createdAt.toLocaleDateString()}
+        </span>
+      </div>
+      <p className="truncate pt-2 text-lg font-extralight">
+        {mailData.subject}
+      </p>
+    </Link>
+  );
+}
--- src/app/mail/[mail]/page.tsx
+++ src/app/mail/[mail]/page.tsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
 
-import { getMailData } from "~/server/queries"
+import { getMailData } from "~/server/queries";
 
 import { Button } from "~/components/ui/button";
+import MailCard from "./mail-card";
 import MailInput from "~/app/_components/mail-input";
 import { Info, RotateCcw } from "lucide-react";
 
@@ -13,7 +14,6 @@ interface MailPageProps {
 export default async function MailPage({ params }: MailPageProps) {
   const mail = params.mail + "@catdns.in";
   const mailData = await getMailData(mail);
-  console.log(mailData)
 
   return (
     <main className="mx-4 mb-14 mt-6 flex flex-col items-center justify-center gap-6 md:mx-[200px] md:mt-10 lg:mx-[300px] xl:mx-[400px] 2xl:mx-[700px]">
@@ -35,86 +35,11 @@ export default async function MailPage({ params }: MailPageProps) {
             Total Emails: 0
           </span>
         </p>
-        <Link
-          href={"/inbox/12314"}
-          className="rounded-md border p-4 hover:bg-primary/20"
-        >
-          <div className="flex items-start justify-between">
-            <div className="flex flex-col">
-              <span className="truncate font-bold">example@thing.com</span>
-              <span className="truncate text-sm">Yazan Bytes</span>
-            </div>
-            <span className="text-sm font-semibold">10/12/2022</span>
-          </div>
-          <p className="truncate pt-2 text-lg font-extralight">
-            Your code is 123456
-          </p>
-        </Link>
-        {/*  */}
-        <div className="rounded-md border p-4">
-          <div className="flex items-start justify-between">
-            <div className="flex flex-col">
-              <span className="truncate font-bold">example@thing.com</span>
-              <span className="truncate text-sm">Yazan Bytes</span>
-            </div>
-            <span className="text-sm font-semibold">10/12/2022</span>
-          </div>
-          <p className="truncate pt-2 text-lg font-extralight">
-            Your code is 123456
-          </p>
-        </div>
-        {/*  */}
-        <div className="rounded-md border p-4">
-          <div className="flex items-start justify-between">
-            <div className="flex flex-col">
-              <span className="truncate font-bold">example@thing.com</span>
-              <span className="truncate text-sm">Yazan Bytes</span>
-            </div>
-            <span className="text-sm font-semibold">10/12/2022</span>
-          </div>
-          <p className="truncate pt-2 text-lg font-extralight">
-            Your code is 123456
-          </p>
-        </div>
-        {/*  */}
-        <div className="rounded-md border p-4">
-          <div className="flex items-start justify-between">
-            <div className="flex flex-col">
-              <span className="truncate font-bold">example@thing.com</span>
-              <span className="truncate text-sm">Yazan Bytes</span>
-            </div>
-            <span className="text-sm font-semibold">10/12/2022</span>
-          </div>
-          <p className="truncate pt-2 text-lg font-extralight">
-            Your code is 123456
-          </p>
-        </div>
-        {/*  */}
-        <div className="rounded-md border p-4">
-          <div className="flex items-start justify-between">
-            <div className="flex flex-col">
-              <span className="truncate font-bold">example@thing.com</span>
-              <span className="truncate text-sm">Yazan Bytes</span>
-            </div>
-            <span className="text-sm font-semibold">10/12/2022</span>
-          </div>
-          <p className="truncate pt-2 text-lg font-extralight">
-            Your code is 123456
-          </p>
-        </div>
-        {/*  */}
-        <div className="rounded-md border p-4">
-          <div className="flex items-start justify-between">
-            <div className="flex flex-col">
-              <span className="truncate font-bold">example@thing.com</span>
-              <span className="truncate text-sm">Yazan Bytes</span>
-            </div>
-            <span className="text-sm font-semibold">10/12/2022</span>
-          </div>
-          <p className="truncate pt-2 text-lg font-extralight">
-            Your code is 123456
-          </p>
-        </div>
+        {mailData
+          ? mailData.mails.map((mail) => (
+              <MailCard key={mail.id} mailData={mail} />
+            ))
+          : null}
       </section>
     </main>
   );
