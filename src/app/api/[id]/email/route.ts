3b1a145b3b503ec94c50f7e7f1797d548a7e2134
TeaByte <yazan4code@gmail.com>
2024-05-21 16:58:29 +0300

    Temp fix to email api

--- src/app/api/[id]/email/route.ts
+++ src/app/api/[id]/email/route.ts
@@ -5,20 +5,27 @@ interface APIProps {
   params: { id: string };
 }
 
+const badResponse = { ok: false, error: "mail not found" };
+
 export async function GET(request: NextRequest, { params }: APIProps) {
-  if(!params.id.includes("@")) {
-    params.id = params.id + "@catway.org"
+  if (!params.id.includes("@")) {
+    params.id = params.id + "@catway.org";
   }
-  
+
   const data = await getMailData(params.id);
 
   if (!data) {
-    return NextResponse.json(
-      { ok: false, error: "mail not found" },
-      {
-        status: 403,
-      },
-    );
+    return NextResponse.json(badResponse, {
+      status: 403,
+    });
+  }
+
+  // TEMP FIX
+  // TODO: MAKE DB DELETE EXPIRE MAILBOX
+  if (data.mails.length == 0) {
+    return NextResponse.json(badResponse, {
+      status: 403,
+    });
   }
 
   return NextResponse.json({ ok: true, data });
